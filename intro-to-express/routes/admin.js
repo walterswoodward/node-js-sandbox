e0ed54b3140e7bc4e57feaf360f0f6ab97991a33
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-25 16:03:08 -0700

    Adds two new view files for shop and add-product pages

--- intro-to-express/routes/admin.js
+++ intro-to-express/routes/admin.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res) => {
     res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>');
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
