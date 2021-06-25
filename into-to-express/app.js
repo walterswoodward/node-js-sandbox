0deb723f56b5698ec756fb29bea154c261494b2d
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-25 15:31:24 -0700

    Adds two new routes /add-product and /product - Also adds body-parser

--- into-to-express/app.js
+++ into-to-express/app.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the Middleware");
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use('/', (req, res, next) => {
+    console.log("This Middleware always runs");
     next();
 });
 
-app.use((req, res, next) => {
-    console.log("In another the Middleware");
+app.use('/add-product', (req, res) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+// Note that first param is not an exact match but "must begin with"
+//  -- so the order of these app.use blocks matters
+// -- 
+app.use('/', (_req, res) => {
     res.send('<h1>Hello from Express!</h1>');
 });
+
 
 app.listen(3000);
