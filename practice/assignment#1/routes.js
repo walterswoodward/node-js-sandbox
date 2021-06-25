7afdb9a448ab29b3f3b780f1543d0ee94a049fe4
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-25 13:53:18 -0700

    Adds changes made to assignment1

--- practice/assignment#1/routes.js
+++ practice/assignment#1/routes.js
@@ -1,23 +1,46 @@
 const fs = require('fs');
 
-const requestHandler = (req,res) => {
+const requestHandler = (req, res) => {
     const url = req.url
     if (url === '/') {
-        fs.readFile('hello.html',function(err,data){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(data);
-            res.end();
-          });
-    }
-    if (url === '/users') {
-        fs.readFile('users.html',function(err,data){
-            res.writeHead(200,{'Content-Type':'text/html'});
+        fs.readFile('./views/index.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             res.end();
         });
     }
+    if (url === '/users') {
+        fs.readFile('./views/users.html', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    }
+    if (url === '/create-user' && req.method === "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const username = parsedBody.split('=')[1];
+            console.log(username + " added!");
+
+            fs.appendFile('users.txt', "\n" + username, (err) => {
+                if (!err) {
+                    res.statusCode = 302;
+                    res.setHeader('Location', '/users'); // redirect to /users
+                    console.log('success!');
+                    return res.end();
+                } else {
+                    console.error(err);
+                }
+            })
+        });
+    }
+
 }
 
-module.exports =  {
+module.exports = {
     handler: requestHandler
 };
