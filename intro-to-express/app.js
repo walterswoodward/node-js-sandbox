c71d70a624340b75860251eb22a9f23739083dc7
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-27 21:18:26 -0700

    Adds 404.html view renders in final fallback route

--- intro-to-express/app.js
+++ intro-to-express/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -17,7 +19,7 @@ app.use(shopRoutes);
 
 // Handle 404
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
